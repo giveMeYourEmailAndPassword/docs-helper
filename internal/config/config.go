dfb44c41ccdd3745fbe3758091a8670f2d96fb93
giveMeYourEmailAndPassword <vvvvv8206@gmail.com>
2026-07-18 19:51:24 +0600

    Fix: QDRANT_URL to REST port 6333 instead of gRPC 6334

--- internal/config/config.go
+++ internal/config/config.go
@@ -21,7 +21,7 @@ type Config struct {
 func Load() (*Config, error) {
 	cfg := &Config{
 		DatabaseURL:    envOrDefault("DATABASE_URL", "postgres://docs:docs@localhost:5432/docs_helper?sslmode=disable"),
-		QdrantURL:      envOrDefault("QDRANT_URL", "http://localhost:6334"),
+		QdrantURL:      envOrDefault("QDRANT_URL", "http://localhost:6333"),
 		S3Endpoint:     envOrDefault("S3_ENDPOINT", "localhost:9000"),
 		S3AccessKey:    envOrDefault("S3_ACCESS_KEY", "minioadmin"),
 		S3SecretKey:    envOrDefault("S3_SECRET_KEY", "minioadmin"),
